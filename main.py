ae8fcbffc25dcae3edaf624d876d0ae41759c152
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-08 21:10:44 +0530

    add data and button logic

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+# ---------------------------- DATA ------------------------------- #
 
+words_data = pd.read_csv('data/french_words.csv')
+words_data_list = pd.DataFrame.to_dict(words_data, orient="records")
+
+
+def next_card():
+    word = random.choice(words_data_list)
+    canvas.itemconfig(canvas_title, text='French')
+    canvas.itemconfig(canvas_word, text=word['French'])
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -23,13 +34,14 @@ canvas_title = canvas.create_text(200, 75, text='', font=('Ariel', 20, 'italic')
 canvas_word = canvas.create_text(200, 131, text='', font=('Ariel', 30, 'bold'))
 
 # Buttons
-right_button = Button(image=right, command='', highlightthickness=0, borderwidth=0)
-unknown_button = Button(image=wrong, command='', highlightthickness=0, borderwidth=0)
+right_button = Button(image=right, command=next_card, highlightthickness=0, borderwidth=0)
+unknown_button = Button(image=wrong, command=next_card, highlightthickness=0, borderwidth=0)
 
 # Layout
 canvas.grid(row=0, column=0, columnspan=2)
 unknown_button.grid(row=1, column=0, pady=20)
 right_button.grid(row=1, column=1, pady=20)
 
+next_card()
 
 window.mainloop()
